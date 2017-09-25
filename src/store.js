027f72916446944a1cb086851245490acf85f211
Phobos2350 <flighty02@gmail.com>
2017-09-25 15:28:44 +0100

    Changed button colours to have better contrast

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
+import Raven from "raven-js";
 import { routerMiddleware } from 'react-router-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import history from './history';
 import reducers from './reducers';
+import createRavenMiddleware from "raven-for-redux";
 
+Raven.config('https://e6a467fdb39f4286a9c2112ae69b6fb4@sentry.io/217280').install()
 
 export default (initialState = {}) => {
-  let middleware = applyMiddleware(thunk, routerMiddleware(history));
+  let middleware = applyMiddleware(createRavenMiddleware(Raven), thunk, routerMiddleware(history));
 
   if (process.env.NODE_ENV !== 'production') {
     const devToolsExtension = window.devToolsExtension;
